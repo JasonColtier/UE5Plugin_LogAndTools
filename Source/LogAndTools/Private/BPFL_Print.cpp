352d756a4cad968166887c5fbdb020c7ac371156
Jason <jasoncoltier@gmail.com>
2024-09-04 15:49:27 +0200

    add pi print

---
+++ Source/LogAndTools/Private/BPFL_Print.cpp
@@ -0,0 +1,33 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "BPFL_Print.h"
+
+#include "Kismet/KismetSystemLibrary.h"
+
+void UBPFL_Print::PI_Print(const UObject* WorldContextObject, FString String1, FString String2, ELogEnum LogCategory, bool bPrintToScreen, bool bPrintToLog, FLinearColor TextColor, float Duration)
+{
+	FString SourceObjectPrefix = FString::Printf(TEXT("[%s] "), *GetNameSafe(WorldContextObject));
+	FText   text               = FText::AsCultureInvariant(String1.Append(String2));
+	switch (LogCategory)
+	{
+	case PI_Debug:
+		UKismetSystemLibrary::PrintText(WorldContextObject, text, bPrintToScreen, bPrintToLog, TextColor, Duration);
+		break;
+	case PI_Warning:
+		{
+			UKismetSystemLibrary::PrintText(WorldContextObject, text, bPrintToScreen, false, TextColor, Duration);
+			const FString FinalLogString = SourceObjectPrefix + *text.ToString();
+			UE_LOG(LogBlueprint, Warning, TEXT("%s"), *FinalLogString);
+		}
+
+		break;
+	case PI_Error:
+		UKismetSystemLibrary::PrintText(WorldContextObject, text, bPrintToScreen, false, TextColor, Duration);
+
+		const FString FinalLogString2 = SourceObjectPrefix + *text.ToString();
+		UE_LOG(LogBlueprint, Error, TEXT("%s"), *FinalLogString2);
+		UE_LOG(LogBlueprint, Error, TEXT("Callstack : \n %s"), *FFrame::GetScriptCallstack());
+		break;
+	}
+}
---
+++ Source/LogAndTools/Public/BPFL_Print.h
@@ -0,0 +1,32 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "BPFL_Print.generated.h"
+
+/**
+ * 
+ */
+
+UENUM()
+enum ELogEnum
+{
+	PI_Debug = 0,
+	PI_Warning = 1,
+	PI_Error = 2,
+};
+
+UCLASS()
+class LOGANDTOOLS_API UBPFL_Print : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+
+public:
+	
+	//un print custom qui rajoute la possibilité de print un warning ou une erreur depuis le BP et d'utiliser 2 strings qui seront concaténées
+	UFUNCTION(BlueprintCallable, meta=(WorldContext="WorldContextObject", CallableWithoutWorldContext, Keywords = "log print", AdvancedDisplay = "2", DevelopmentOnly), Category="Utilities|String")
+	static void PI_Print(const UObject* WorldContextObject, FString String1, FString String2, ELogEnum LogCategory, bool bPrintToScreen = true, bool bPrintToLog = true, FLinearColor TextColor = FLinearColor(0.0, 0.66, 1.0), float Duration = 2.f);
+
+};
