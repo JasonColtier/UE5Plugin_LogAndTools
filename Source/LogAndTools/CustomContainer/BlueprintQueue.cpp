f5401872615086d6f3e64d09fe385ea9c6df5309
Jason <jasoncoltier@gmail.com>
2024-09-19 10:51:33 +0200

    add blueprint queue

---
+++ Source/LogAndTools/CustomContainer/BlueprintQueue.cpp
@@ -0,0 +1,38 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "BlueprintQueue.h"
+
+bool UBlueprintQueue::Enqueue(UObject* ObjectToQueue)
+{
+	if (IsValid(ObjectToQueue))
+	{
+		return ObjectsQueue.Enqueue(ObjectToQueue);
+	}
+	return false;
+}
+
+bool UBlueprintQueue::Dequeue(UObject*& DequeuedObject)
+{
+	return ObjectsQueue.Dequeue(DequeuedObject);
+}
+
+void UBlueprintQueue::Empty()
+{
+	return ObjectsQueue.Empty();
+}
+
+bool UBlueprintQueue::IsEmpty()
+{
+	return ObjectsQueue.IsEmpty();
+}
+
+bool UBlueprintQueue::Peek(UObject*& Object)
+{
+	return ObjectsQueue.Peek(Object);
+}
+
+bool UBlueprintQueue::Pop()
+{
+	return ObjectsQueue.Pop();
+}
---
+++ Source/LogAndTools/CustomContainer/BlueprintQueue.h
@@ -0,0 +1,43 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Object.h"
+#include "BlueprintQueue.generated.h"
+
+/**
+ * 
+ */
+UCLASS(BlueprintType)
+class LOGANDTOOLS_API UBlueprintQueue : public UObject
+{
+	GENERATED_BODY()
+
+private:
+	TQueue<UObject*> ObjectsQueue;
+
+public:
+	
+	/**
+	 * adds and object in last position
+	 * @param ObjectToQueue 
+	 */
+	UFUNCTION(BlueprintCallable,Category = "Queue")
+	bool Enqueue(UObject* ObjectToQueue);
+	
+	UFUNCTION(BlueprintCallable, Category = "Queue")
+	bool Dequeue(UObject*& DequeuedObject);
+	
+	UFUNCTION(BlueprintCallable, Category = "Queue")
+	void Empty();
+	
+	UFUNCTION(BlueprintPure, Category = "Queue")
+	bool IsEmpty();
+	
+	UFUNCTION(BlueprintCallable, Category = "Queue")
+	bool Peek(UObject*& Object);
+	
+	UFUNCTION(BlueprintCallable, Category = "Queue")
+	bool Pop();
+};
